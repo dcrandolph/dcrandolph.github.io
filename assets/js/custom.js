b955a915efb43be1a3c953d93ef4509e600f7149
DC Randolph <dieter@waypastokay.com>
2026-07-09 14:03:58 -0400

    open external links in new window

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,14 @@
-console.log('javascript is working.')
+function externalNewWindow() {
+  const links = document.querySelectorAll('a[href]');
+  for (const link of links) {
+    if (link.hostname !== location.hostname) {
+      link.target = '_blank';
+      link.rel = 'noopener';
+    }
+  }
+}
+
+window.addEventListener("load", () => {
+  externalNewWindow();
+});
+
