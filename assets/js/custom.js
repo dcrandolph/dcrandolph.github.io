61365d7c4a5d2d234bb02eb57296583ee76ea796
DC Randolph <dieter@waypastokay.com>
2026-07-10 14:06:37 -0400

    unobfuscating email link

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,20 @@
-function externalNewWindow() {
+function linkMassage() {
   const links = document.querySelectorAll('a[href]');
   for (const link of links) {
+    const href = link.getAttribute('href');
+    // handle obfuscated email links
+    if (href.startsWith('mailto:') && href.includes(' AT ')) {
+      link.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        const obfuscated = href.replace('mailto:', '');
+        const realEmail = obfuscated
+          .replace(/\sAT\s/gi, '@')
+          .replace(/\sDOT\s/gi, '.');
+        window.location.href = `mailto:${realEmail}`;
+      });
+      continue;
+    }
+    // open external links in a new window
     if (link.hostname !== location.hostname) {
       link.target = '_blank';
       link.rel = 'noopener';
@@ -9,6 +23,6 @@ function externalNewWindow() {
 }
 
 window.addEventListener("load", () => {
-  externalNewWindow();
+  linkMassage();
 });
 
